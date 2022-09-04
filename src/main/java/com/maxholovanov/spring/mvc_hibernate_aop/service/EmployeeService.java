06df734586c4c51a8beb13b501e741902cf44ea4
maxikyo <holovanovmax@gmail.com>
2022-09-04 18:22:59 +0200

    create-update for DAO(Services)

--- src/main/java/com/maxholovanov/spring/mvc_hibernate_aop/service/EmployeeService.java
+++ src/main/java/com/maxholovanov/spring/mvc_hibernate_aop/service/EmployeeService.java
@@ -1,4 +1,10 @@
 package com.maxholovanov.spring.mvc_hibernate_aop.service;
 
+import com.maxholovanov.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
 public interface EmployeeService {
+
+    public List<Employee> getAllEmployees();
 }

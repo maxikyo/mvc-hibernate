9cfbd2c301d70e403ea41a7a8825f11fa73653b5
maxikyo <holovanovmax@gmail.com>
2022-10-02 22:02:16 +0200

    Create update button(finish l-5)

--- src/main/java/com/maxholovanov/spring/mvc_hibernate_aop/dao/EmployeeDAO.java
+++ src/main/java/com/maxholovanov/spring/mvc_hibernate_aop/dao/EmployeeDAO.java
@@ -6,4 +6,8 @@ import java.util.List;
 
 public interface EmployeeDAO {
     public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+   public Employee getEmployee(int id);
 }
